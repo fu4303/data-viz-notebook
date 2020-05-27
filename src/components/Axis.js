0c6a71bb7bac193230f39a483276b2b1b5a1297d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-27 16:08:05 +0100

    create vertical axis

--- src/components/Axis.js
+++ src/components/Axis.js
@@ -2,15 +2,22 @@ import React from "react";
 
 const axisDirection = {
   x: AxisBottom,
-  // y: AxisLeft,
+  y: AxisLeft,
 };
+
 const Axis = ({ dimensions, direction, ...props }) => {
   const Component = axisDirection[direction];
   if (!Component) return null;
   return <Component {...props} dimensions={dimensions} />;
 };
 
-function AxisBottom({ dimensions, label, scale, formatTick, ticks }) {
+function AxisBottom({
+  dimensions,
+  label = "",
+  scale,
+  formatTick = (d) => d,
+  ticks,
+}) {
   return (
     <g transform={`translate(0, ${dimensions.boundedHeight})`}>
       <line x1={0} x2={dimensions.boundedWidth} y1={0} y2={0} stroke="black" />
@@ -33,6 +40,44 @@ function AxisBottom({ dimensions, label, scale, formatTick, ticks }) {
       ))}
       <text
         transform={`translate(${dimensions.boundedWidth / 2}, 35)`}
+        style={{ color: "black", textAnchor: "middle", fontSize: "12px" }}
+      >
+        {label}
+      </text>
+    </g>
+  );
+}
+function AxisLeft({
+  dimensions,
+  label = "",
+  scale,
+  formatTick = (d) => d,
+  ticks,
+}) {
+  return (
+    <g transform={`translate(${0}, 0)`}>
+      <line x1={0} x2={0} y1={dimensions.boundedHeight} y2={0} stroke="black" />
+      {ticks.map((tick, i) => (
+        <React.Fragment key={tick}>
+          <line
+            x1={0}
+            x2={-5}
+            y1={scale(tick)}
+            y2={scale(tick)}
+            stroke="black"
+          />
+          <text
+            transform={`translate(-15, ${scale(tick)})`}
+            style={{ fontSize: "10px", textAnchor: "middle" }}
+          >
+            {formatTick(tick)}
+          </text>
+        </React.Fragment>
+      ))}
+      <text
+        transform={`translate(-40, ${
+          dimensions.boundedHeight / 2
+        }) rotate(-90)`}
         style={{ color: "black", textAnchor: "middle", fontSize: "12px" }}
       >
         {label}
