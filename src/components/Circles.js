d890f5a0ba3d2898626b066d52f2e2f2ff1baf2c
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-29 18:20:08 +0100

    start creating tooltip

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Circles = ({ data, xAccessor, yAccessor, colorAccessor }) => {
+  const [isShown, setIsShown] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
   return (
     <>
       {data.map((d, i) => (
@@ -10,9 +12,38 @@ const Circles = ({ data, xAccessor, yAccessor, colorAccessor }) => {
           cy={yAccessor(d)}
           r={5}
           fill={colorAccessor(d)}
+          onMouseEnter={() => {
+            setTooltipPosition({ x: xAccessor(d), y: yAccessor(d) });
+            return setIsShown(true);
+          }}
+          onMouseLeave={() => setIsShown(false)}
         />
       ))}
+      <Tooltip
+        x={tooltipPosition.x}
+        y={tooltipPosition.y}
+        width={100}
+        height={100}
+      />
     </>
+  );
+};
+
+const Tooltip = ({ x, y, width, height }) => {
+  return (
+    <g transform={`translate(${x - width / 2} ${y - height - 10})`}>
+      <rect width={width} height={height} fill="white" />
+      <text
+        transform={`translate(${width / 2} ${height / 2})`}
+        style={{
+          color: "black",
+          textAnchor: "middle",
+          fontSize: "12px",
+        }}
+      >
+        tooltip
+      </text>
+    </g>
   );
 };
 
