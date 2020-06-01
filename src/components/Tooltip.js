c1cf5ae51c7b848c3b2bcc59d21f4b4663ca8053
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-06-01 15:12:21 +0100

    display content, fix tooltip coordinates

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,32 +1,56 @@
 import React from "react";
 
-const Tooltip = ({ x, y, width, height, isShown = false }) => {
+const Tooltip = ({ x, y, width, height, isShown = false, content }) => {
+  let boxX1, boxX2, boxY1, boxY2;
+  let pointRight = `${x + 5},${y - 10}`;
+  let pointBottom = `${x},${y - 5}`;
+  let pointLeft = `${x - 5},${y - 10}`;
+
+  if (x < width / 2) {
+    boxX1 = x / 2;
+    boxX2 = x / 2 + width;
+    boxY1 = y - 10 - height;
+    boxY2 = y - 10;
+  } else {
+    boxX1 = x - width / 2;
+    boxX2 = x + width / 2;
+    boxY1 = y - 10 - height;
+    boxY2 = y - 10;
+  }
+  const topLeft = `${boxX1},${boxY1}`;
+  const bottomLeft = `${boxX1},${boxY2}`;
+  const topRight = `${boxX2},${boxY1}`;
+  const bottomRight = `${boxX2},${boxY2}`;
+
   const points = [
-    `${x - width / 2},${y - 10 - height}`,
-    `${x + width / 2},${y - 10 - height}`,
-    `${x + width / 2},${y - 10}`,
-    `${x + 5},${y - 10}`,
-    `${x},${y - 5}`,
-    `${x - 5},${y - 10}`,
-    `${x - width / 2},${y - 10}`,
+    topLeft,
+    topRight,
+    bottomRight,
+    pointRight,
+    pointBottom,
+    pointLeft,
+    bottomLeft,
   ].join(" ");
 
   if (!isShown) return null;
   return (
     <g>
       <polygon points={points} stroke="blue" strokeWidth="1" fill="white" />
-      <text
-        x={x}
-        y={y - 10 - height / 2}
-        style={{
-          color: "black",
-          textAnchor: "middle",
-          fontSize: "12px",
-          alignmentBaseline: "middle",
-        }}
-      >
-        tooltip
-      </text>
+      {content.map((d, i) => (
+        <text
+          key={i}
+          x={boxX1 + width / 2}
+          y={y - 20 - height / 2 + i * 12}
+          style={{
+            color: "black",
+            textAnchor: "middle",
+            fontSize: "12px",
+            alignmentBaseline: "middle",
+          }}
+        >
+          {d}
+        </text>
+      ))}
     </g>
   );
 };
